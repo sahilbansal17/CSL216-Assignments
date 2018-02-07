84eb5d5702825a538a85d6751d3d1e629d9e29a5
Sahil Bansal <shlbnsl843@gmail.com>
2018-02-07 16:58:22 +0530

    added A2 instructions

---
+++ A2/lfsr.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+int main(void){
+    uint16_t start_state = 0xACE1u;  /* Any nonzero start state will work. */
+
+    uint16_t lfsr = start_state;
+
+    uint16_t bit;                    /* Must be 16bit to allow bit<<15 later in the code */
+
+    unsigned period = 0;
+
+    do{
+        /* taps: 16 14 13 11; feedback polynomial: x^16 + x^14 + x^13 + x^11 + 1 */
+
+        bit  = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) ) & 1;
+
+        lfsr =  (lfsr >> 1) | (bit << 15);
+
+        if (period < 5) printf("lfsr=%x\n", lfsr);
+
+        ++period;
+    }while (lfsr != start_state);
+
+    printf("Post termination lfsr=%x", lfsr);
+
+    return 0;
+}
